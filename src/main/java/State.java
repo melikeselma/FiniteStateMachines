a075229c71ee915312fe61aea61db4fe57030deb
Melike <124033708+melikeselma@users.noreply.github.com>
2025-04-29 18:24:50 +0300

    Update State.java

--- src/main/java/State.java
+++ src/main/java/State.java
@@ -14,6 +14,7 @@ public class State {
 
     @Override
     public boolean equals(Object o) {
+        return false;
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         State state = (State) o;
